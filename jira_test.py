e329b358d273cc8401cb533f27fbf0019ef05179
Martin Ulmschneider <mu@ocedo.com>
2014-10-10 13:55:40 +0200

    Example jira usage

--- jira_test.py
+++ jira_test.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
+#click?
 
 from jira.client import JIRA
+import inspect
+import pprint
 
 
 jira = JIRA(basic_auth=('Alice', 'alice'), options={ 'server': 'https://ulmi-test2.atlassian.net'})
@@ -8,4 +11,6 @@ issue = jira.issue('DEV-2')
 print issue.fields.project.key # 'JRA' 
 print issue.fields.issuetype.name # 'New Feature'
 print issue.fields.reporter.displayName # 'Mike Cannon-Brookes [Atlassian]'
-print issue.fields
+#issue.update(customfield_10025=['develop', 'next', 'stage1'])
+pp.pprint(inspect.getmembers(issue.fields()))
+#new_issue= jira.create_issue(project={'key' : 'DEV'}, summary='Auto-created bug', description='This bug was created via the jira python API', issuetype={'name': 'Bug'})
